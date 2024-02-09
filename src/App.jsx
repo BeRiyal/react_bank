39c2d5a905779d426bca0e744c290ce0be351c41
BeReal <rpatel5884@conestogac.on.ca>
2024-02-09 15:24:08 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./style"
+const App =() => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className="{`${styles.paddingX} ${styles.flexCenter}`}">
+        
+      </div>
+    </div>
+  )
+};
+
+export default App;
